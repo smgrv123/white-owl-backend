5529ee5ad827bfa27078ad726c38d644accb3423
Sumrit Grover <sumritgrover1@gmail.com>
2024-05-14 16:49:41 +0530

    add query params for the get request

--- src/routes/films/get.js
+++ src/routes/films/get.js
@@ -5,12 +5,18 @@ import { films } from '../../schema/films.js';
 const getFilmsRouter = express.Router();
 
 getFilmsRouter.get('/', async (req, res) => {
+  const categoryName = req.query.categoryName;
   try {
     const getFilmDetails = await films.find();
-    if (getFilmDetails.length > 0) res.json(getFilmDetails);
-    else res.send('No Films Found');
+    if (categoryName) {
+      const getCategoryFilmDetails = getFilmDetails.filter((i) => i.category === categoryName);
+      res.send(getCategoryFilmDetails);
+    } else {
+      if (getFilmDetails.length > 0) res.json(getFilmDetails);
+      else res.send('No Films Found');
+    }
   } catch (error) {
-    res.send('Error', error);
+    res.send(error);
   }
 });
 
